5e53f1f8d9260da90c7afa4900e376586a4f3e4a
tjaRoxasXIII <tjaRoguetech@gmail.com>
2026-08-30 15:52:34 -0400

    W01 Learning activity for node.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
