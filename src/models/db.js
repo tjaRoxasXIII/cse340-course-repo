a4bdb09a8aedef9c8540f5cf2cc3f69db617568c
tjaRoxasXIII <tjaRoguetech@gmail.com>
2026-09-09 18:50:55 -0400

    reverting changes as error was found elsewhere

--- src/models/db.js
+++ src/models/db.js
@@ -13,10 +13,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: {
-        require: true,
-        rejectUnauthorized: false
-    }
+    ssl: true
 });
 
 /**
