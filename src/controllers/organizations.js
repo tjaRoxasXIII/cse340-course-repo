ecded8849aef43451a6a4035b5e0593f1900ca92
tjaRoxasXIII <tjaRoguetech@gmail.com>
2026-09-15 19:13:47 -0400

    Adding routes and query params for detailed viewing of organizations and specific projects within

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,4 +1,5 @@
-import { getAllOrganizations } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
 
 const organizationsPage = async (req, res) => {
     const organizations = await getAllOrganizations();
@@ -7,4 +8,13 @@ const organizationsPage = async (req, res) => {
     res.render('organizations', { title, organizations });
 };
 
-export { organizationsPage };
+const showOrganizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organization', {title, organizationDetails, projects});
+};
+
+export { organizationsPage, showOrganizationDetailsPage };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { homePage } from './controllers/index.js';
 import { projectsPage } from './controllers/projects.js';
 import { categoriesPage } from './controllers/categories.js';
-import { organizationsPage } from './controllers/organizations.js';
+import { organizationsPage, showOrganizationDetailsPage } from './controllers/organizations.js';
 import { testError } from './controllers/errors.js';
 
 const router = express.Router();
@@ -12,6 +12,7 @@ router.get('/', homePage);
 router.get('/projects', projectsPage);
 router.get('/categories', categoriesPage);
 router.get('/organizations', organizationsPage);
+router.get('/organization/:id', showOrganizationDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testError);
